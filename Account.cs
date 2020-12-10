272c070e62e8b1d17498d40cedce9ca050467e14
pravalika-prog <68769158+pravalika-prog@users.noreply.github.com>
2020-12-10 17:06:54 +0530

    Add files via upload

---
+++ Account.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BankAccount
+{
+ public   class Account
+    {
+        public double balance;
+        public Account()
+        {
+            balance = 500;
+        }
+        public double deposit()
+        {
+            double Newbalance;
+            double deposit;
+            Console.WriteLine("enter the amaount to deposit");
+            deposit = double.Parse(Console.ReadLine());
+            Newbalance = balance + deposit;
+            return Newbalance;
+        }
+        public double Withdrawl()
+        {
+            double Newbalance;
+            double Withdrawl;
+            Console.WriteLine("enter the amaount to withdrawl");
+            Withdrawl = double.Parse(Console.ReadLine());
+            Newbalance = balance - Withdrawl;
+            return Newbalance;
+        }
+    }
+}
---
+++ Money.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankAccount
+{
+    class Money
+    {
+        static void Main()
+        {
+            double  Newbalance;
+            string balance;
+            Account ac = new Account();
+            Console.WriteLine("your current balance is 500 ");
+            Console.WriteLine("press d for deposit,press w for withdrwal ");
+            balance = Console.ReadLine();
+            if (balance == "d")
+            {
+                Newbalance = ac.deposit();
+                Console.WriteLine( "you are balance is:{0}", Newbalance);
+            }
+           else if(balance== "w")
+            {
+                Newbalance = ac.Withdrawl();
+                Console.WriteLine("you are balance is:{0}", Newbalance);
+                    
+                    
+            }
+
+        }
+    }
+}
